ba470f302c2998e94877bd3520e6bde121b747d1
Adam Yao <Adamyao@yeah.net>
2019-12-30 13:57:30 +0800

    [DEV] types of button, base color for all themes

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,6 @@ enum themes {
   DARK = 'dark'
 };
 
-enum SIZE {
-  SMALL = 'S',
-  NORMAL = 'N',
-  LARGE = "L"
-};
-
 const initialState = {
   theme: themes.DARK
 };
@@ -29,7 +23,36 @@ const App: React.FC = () => {
         <VCButton text='ADAM' icon='ic_accessibility_p' onClick={() => { alert('Clicked GOV\'DAN'); }} />
         <VCButton text='ADAM' icon='ic_accessibility_p' onClick={() => { alert('Clicked GOV\'DAN'); }} size='L'/>
         <VCButton text='ADAM' icon='ic_accessibility_p' onClick={() => { alert('Clicked GOV\'DAN'); }} disabled={true} />
-
+        <br/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Filled' onClick={() => { alert('Clicked GOV\'DAN'); }} size='S'/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Filled' onClick={() => { alert('Clicked GOV\'DAN'); }} />
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Filled' onClick={() => { alert('Clicked GOV\'DAN'); }} size='L'/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Filled' onClick={() => { alert('Clicked GOV\'DAN'); }} disabled={true} />
+        <br/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary' onClick={() => { alert('Clicked GOV\'DAN'); }} size='S'/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary' onClick={() => { alert('Clicked GOV\'DAN'); }} />
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary' onClick={() => { alert('Clicked GOV\'DAN'); }} size='L'/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary' onClick={() => { alert('Clicked GOV\'DAN'); }} disabled={true} />
+        <br/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary_Special' onClick={() => { alert('Clicked GOV\'DAN'); }} size='S'/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary_Special' onClick={() => { alert('Clicked GOV\'DAN'); }} />
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary_Special' onClick={() => { alert('Clicked GOV\'DAN'); }} size='L'/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary_Special' onClick={() => { alert('Clicked GOV\'DAN'); }} disabled={true} />
+        <br/>
+        <VCButton icon='ic_accessibility_p' type='Primary_Special' onClick={() => { alert('Clicked GOV\'DAN'); }} size='S'/>
+        <VCButton icon='ic_accessibility_p' type='Primary_Special' onClick={() => { alert('Clicked GOV\'DAN'); }} />
+        <VCButton icon='ic_accessibility_p' type='Primary_Special' onClick={() => { alert('Clicked GOV\'DAN'); }} size='L'/>
+        <VCButton icon='ic_accessibility_p' type='Primary_Special' onClick={() => { alert('Clicked GOV\'DAN'); }} disabled={true} />
+        <br/>
+        <VCButton icon='ic_accessibility_p' type='Circle' onClick={() => { alert('Clicked GOV\'DAN'); }} size='S'/>
+        <VCButton icon='ic_accessibility_p' type='Circle' onClick={() => { alert('Clicked GOV\'DAN'); }} />
+        <VCButton icon='ic_accessibility_p' type='Circle' onClick={() => { alert('Clicked GOV\'DAN'); }} size='L'/>
+        <VCButton icon='ic_accessibility_p' type='Circle' onClick={() => { alert('Clicked GOV\'DAN'); }} disabled={true} />
+        <br/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary_Special_1' onClick={() => { alert('Clicked GOV\'DAN'); }} size='S'/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary_Special_1' onClick={() => { alert('Clicked GOV\'DAN'); }} />
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary_Special_1' onClick={() => { alert('Clicked GOV\'DAN'); }} size='L'/>
+        <VCButton text='ADAM' icon='ic_accessibility_p' type='Primary_Special_1' onClick={() => { alert('Clicked GOV\'DAN'); }} disabled={true} />
         <div className='themeSwitchBtn'>
           <button onClick={() => { setTheme(themes.LIGHT); }}>light</button>
           <button onClick={() => { setTheme(themes.DARK); }} >dark</button>
--- src/components/VCButton/VCButton.tsx
+++ src/components/VCButton/VCButton.tsx
@@ -2,15 +2,21 @@ import React, { MouseEvent, SFC } from 'react';
 import './VCButton.scss';
 
 enum BTNTYPE {
-    PRIMAY_1 = 'Primay',
-    PRIMAY_2 = 'Primay_Special',
+    PRIMARY_1 = 'Primary',
+    PRIMARY_2 = 'Primary_Special',
+    PRIMARY_3 = 'Primary_Special_1',
+    CIRCLE_1 = 'Circle',
+    CIRCLE_2 = 'Circle_Special',
     LINE = 'Line',
     FILLED = 'Filled'
 };
 
 enum TYPE_CLASSES {
-    PRIMAY_1 = 'bt_primay',
-    PRIMAY_2 = 'bt_primay_s',
+    PRIMARY_1 = 'bt_primary',
+    PRIMARY_2 = 'bt_primary_s',
+    PRIMARY_3 = 'bt_primary_s_1',
+    CIRCLE_1 = 'bt_circle',
+    CIRCLE_2 = 'bt_circle_s',
     LINE = 'bt_line',
     FILLED = 'bt_filled'
 }
@@ -33,19 +39,28 @@ type props = {
 
 export const VCButton: SFC<props> = ({ onClick: handleClick, icon, text, size = SIZE.NORMAL, type = BTNTYPE.LINE, disabled = false, className }) => {
 
+    /** According to the values of type, size, classname, Calc the classes **/
     const calcClasses = (type: string, size: string, className?: string): string => {
         let finalClasses: string = 'VCButton';
         if (type) {
             switch (type) {
-
                 case BTNTYPE.FILLED:
                     finalClasses += ` ${TYPE_CLASSES.FILLED}`;
                     break;
-                case BTNTYPE.PRIMAY_1:
-                    finalClasses += ` ${TYPE_CLASSES.PRIMAY_1}`;
+                case BTNTYPE.PRIMARY_1:
+                    finalClasses += ` ${TYPE_CLASSES.PRIMARY_1}`;
                     break;
-                case BTNTYPE.PRIMAY_2:
-                    finalClasses += ` ${TYPE_CLASSES.PRIMAY_2}`;
+                case BTNTYPE.PRIMARY_2:
+                    finalClasses += ` ${TYPE_CLASSES.PRIMARY_2}`;
+                    break;
+                case BTNTYPE.PRIMARY_3:
+                    finalClasses += ` ${TYPE_CLASSES.PRIMARY_3}`;
+                    break;
+                case BTNTYPE.CIRCLE_1:
+                    finalClasses += ` ${TYPE_CLASSES.CIRCLE_1}`;
+                    break;
+                case BTNTYPE.CIRCLE_2:
+                    finalClasses += ` ${TYPE_CLASSES.CIRCLE_2}`;
                     break;
                 case BTNTYPE.LINE:
                 default:
@@ -68,7 +83,10 @@ export const VCButton: SFC<props> = ({ onClick: handleClick, icon, text, size = 
                     break;
             }
         }
-        if(className){
+        if (!text) {
+            finalClasses += ' onlyIcon';
+        }
+        if (className) {
             finalClasses += ` ${className}`;
         }
         return finalClasses;
